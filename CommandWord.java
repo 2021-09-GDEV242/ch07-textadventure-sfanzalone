24d4eece8fd63ccb5a76edcf0761b96c37df1970
sfanzalone <sfanzalone@gmail.com>
2022-10-24 07:59:10 -0400

    Added the LOOK and EAT commands to class; resolved most of the errors
    I originally had 22 errors upon opening this project, and 13 of them were resolved after adding the LOOK and EAT commands to the CommandWord class.

--- CommandWord.java
+++ CommandWord.java
@@ -2,14 +2,15 @@
  * Representations for all the valid command words for the game
  * along with a string in a particular language.
  * 
- * @author  Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * @author  Salvatore Anzalone
+ * @version 10.24.2022
  */
 public enum CommandWord
 {
     // A value for each command word along with its
     // corresponding user interface string.
-    GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?");
+    GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?"),
+    LOOK("look"), EAT("eat");
     
     // The command string.
     private String commandString;
